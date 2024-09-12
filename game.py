f7a16450d2463d76af6ff0ec5d556d2379167f5b
Pheonixj07 <victoryazuta@gmail.com>
2024-09-12 11:16:02 -0400

    Modified game.py

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ def ask_question(question, answer):
     user_answer = input(question + " ")
     return user_answer.lower() == answer.lower()
 
+# How many galaxies are in the universe
+# Which Console do you think won the console wars?
 def main():
     questions = {
         "What is the capital of France?": "paris",
